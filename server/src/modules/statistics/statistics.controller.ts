9194a817ce1f958ceb9225342e0042a04114a99f
Fatima Ghazzawi <fatimaghazzawi10@gmail.com>
2026-08-02 12:40:06 +0300

    client folder added

--- server/src/modules/statistics/statistics.controller.ts
+++ server/src/modules/statistics/statistics.controller.ts
@@ -1,7 +1,10 @@
 import type { FastifyReply, FastifyRequest } from 'fastify';
 
+import { getAuthUser } from '@common/middleware';
 import { success } from '@common/utils/response';
+import { parseOrThrow } from '@common/validators';
 
+import { statisticsQuerySchema } from './statistics.schema';
 import type { StatisticsService } from './statistics.service';
 
 /**
@@ -9,12 +12,15 @@ import type { StatisticsService } from './statistics.service';
  * Parse/validate input, call the service, return response helpers.
  * No business rules and no direct database/API calls.
  */
-
 export class StatisticsController {
   constructor(private readonly service: StatisticsService) {}
 
-  async readiness(_request: FastifyRequest, _reply: FastifyReply) {
-    return success(this.service.getScaffoldStatus());
+  async getMine(request: FastifyRequest, _reply: FastifyReply) {
+    const auth = getAuthUser(request);
+    const query = parseOrThrow(statisticsQuerySchema, request.query);
+    const statistics = await this.service.getForUser(auth.sub, query.refresh ?? false);
+
+    return success({ statistics });
   }
 }
 
